e437a1a844c6a26cf497e069ec76abf49dfe7739
github username <github email address>
2018-11-28 07:12:48 -0500

    TIPS exercise project euler completed.

--- lib/oo_sum_square_difference.rb
+++ lib/oo_sum_square_difference.rb
@@ -1,1 +1,24 @@
 # Implement your object-oriented solution here!
+class SumSquareDifference
+  # initialize instance variable to array of numbers based on parameter passed
+  def initialize(range)
+    @array = Array(1..range)
+  end
+
+  def difference
+    # initialize class method variables
+    sumOfNumbers = 0
+    sumOfSquares = 0
+
+    # iterate through instance variable array and make computations
+    @array.each do |num|
+      sumOfNumbers = sumOfNumbers + num
+      sumOfSquares = sumOfSquares + (num * num)
+      puts sumOfSquares
+    end
+
+    # return difference of two calculations
+    return (sumOfSquares - (sumOfNumbers * sumOfNumbers)).abs
+  end
+
+end
--- lib/sum_square_difference.rb
+++ lib/sum_square_difference.rb
@@ -1,1 +1,19 @@
 # Implement your procedural solution here!
+def sum_square_difference(range)
+  # initialize array of numbers based on parameter passed
+  array = Array(1..range)
+
+  # initialize variables
+  sumOfNumbers = 0
+  sumOfSquares = 0
+
+  # iterate through and make computations
+  array.each do |num|
+    sumOfNumbers = sumOfNumbers + num
+    sumOfSquares = sumOfSquares + (num * num)
+    puts sumOfSquares
+  end
+
+  # return difference of two calculations
+  return (sumOfSquares - (sumOfNumbers * sumOfNumbers)).abs
+end
